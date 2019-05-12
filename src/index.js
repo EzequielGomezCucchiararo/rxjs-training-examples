1f7ceb5ad6e855d1c39f1cc40352a0d42e686188
Ezequiel Gomez <ezequiel.gomez@softonic.com>
2019-05-12 11:31:26 +0200

    Refactored

--- src/index.js
+++ src/index.js
@@ -1,21 +1,6 @@
-<<<<<<< HEAD
-import { fromEvent } from 'rxjs';
-import { map, scan, tap } from 'rxjs/operators';
-
-const myButtonElement = document.getElementById('myButton');
-const myButton$ = fromEvent(myButtonElement, 'click');
-
-myButton$
-  .pipe(
-    map(() => 1),
-    tap(() => console.log('Button clicked!')),
-    scan((acc, current) => acc + current)
-  )
-  .subscribe(count => {
-    console.log(`Total clicks: ${count}`);
-  });
-=======
 // --- EXAMPLE 1: PURE FUNCTIONS ---
+import { fromEvent } from 'rxjs';
+import { scan } from 'rxjs/operators';
 
 // VANILLA:
 let count = 0;
@@ -26,34 +11,8 @@ document.addEventListener(
 );
 
 // RXJS
-import { fromEvent } from 'rxjs';
-import { scan } from 'rxjs/operators';
-
 fromEvent(document, 'click')
   .pipe(scan(count => count + 1, 0))
-  .subscribe(count => console.log(`Clicked ${count} times`));
-
-// --- EXAMPLE 2: FLOW ---
-// VANILLA:
-let count = 0;
-let rate = 1000;
-let lastClick = Date.now() - rate;
-document.addEventListener('click', () => {
-  if (Date.now() - lastClick >= rate) {
-    console.log(`Clicked ${++count} times`);
-    lastClick = Date.now();
-  }
-});
-
-// RXJS
-import { fromEvent } from 'rxjs';
-import { throttleTime, scan } from 'rxjs/operators';
-
-fromEvent(document, 'click')
-  .pipe(
-    throttleTime(1000),
-    scan(count => count + 1, 0)
-  )
-  .subscribe(count => console.log(`Clicked ${count} times`));
-
->>>>>>> tutorial
+  .subscribe(count => {
+    console.log(`Clicked ${count} times`)
+  });
